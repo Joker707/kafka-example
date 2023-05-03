d8027b7cb875d26d86f874599dca7c5bdd09ecdf
joker <ser.fyodoroff@yandex.ru>
2023-05-03 12:56:17 +0300

    Init project

--- src/main/java/com/example/kafkaexample/KafkaExampleApplication.java
+++ src/main/java/com/example/kafkaexample/KafkaExampleApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class KafkaExampleApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(KafkaExampleApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(KafkaExampleApplication.class, args);
+    }
 
 }
