120eece9148375d7a9ccb6a55ffa2d33a014adee
joker <ser.fyodoroff@yandex.ru>
2023-05-03 13:30:22 +0300

    Topic config added

--- src/main/java/com/joker/kafka/KafkaExampleApplication.java
+++ src/main/java/com/joker/kafka/KafkaExampleApplication.java
@@ -1,4 +1,4 @@
-package com.example.kafkaexample;
+package com.joker.kafka;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
